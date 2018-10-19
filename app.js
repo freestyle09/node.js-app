46f0a8801e774298f62ad9ee622074d2233ab37f
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-19 22:51:44 +0200

    done mongoose

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+//
+// emitter.on('logging', (arg) => {
+//     console.log(arg);
+// });
+//
+// emitter.emit('logging', {data: 'message'})
 
-emitter.on('logging', (arg) => {
-    console.log(arg);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-emitter.emit('logging', {data: 'message'})
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
