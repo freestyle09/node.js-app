df175cd18c364b5cbca74618f27e2fa7fc6e0b3d
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-22 17:27:28 +0200

    done mongoose

--- app.js
+++ app.js
@@ -7,21 +7,31 @@
 //
 // emitter.emit('logging', {data: 'message'})
 
-const express = require("express");
-const app = express();
-const logger = require("./logging");
+// const express = require("express");
+// const app = express();
+// const logger = require("./logging");
+//
+// app.use(express.json());
+// app.use(logger);
+//
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+//
+// app.get("/api/courses", (req, res) => {
+//   res.send([1, 2, 3]);
+// });
+//
+// const port = process.env.PORT || 3000;
+//
+// app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-app.use(express.json());
-app.use(logger);
+const mongoose =require('mongoose');
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to mongoDB...'))
+    .catch(err => console.log(new Error('Colud not connect to mongoDB', err)));
 
-app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const imageSchema = new mongoose.Schema({
+    url: String
+})
